054fb4cbd10d65dcd58311aada0982c6cad01507
JoelEspinal <j.espinalcarrasco@gmail.com>
2022-05-27 16:46:30 -0400

    🎨 Move image builder to separate method

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -14,8 +14,27 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Image.network(images[index].url);
+        return buildImage(images[index]);
       },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Column(
+        children: <Widget>[
+          Image.network(image.url),
+          Padding(
+            padding: EdgeInsets.only(bottom: 8.0),
+            child: Text(image.title),
+          ),
+        ],
+      ),
     );
   }
 }
